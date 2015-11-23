ad9c30252891526ccd6d623e9da6ac6a920bacb8
paulvitic <paul@vektortelekom.com>
2015-11-24 00:36:08 +0100

    cleaning code

--- test/model.js
+++ test/model.js
@@ -10,16 +10,20 @@ var model = require('../service/model');
 describe('model', function () {
     it('should return cached token', function (done) {
         var token = '1626c7115914de88590c15a2cb3ba657079b5ba9';
-        model.getAccessToken(token, function(err, result){
-            var assert = (err === null).should.be.true;
-            console.log(assert);
-            if (result){
-                model.getAccessToken(token, function(err, result){
-                    assert = result.cached.should.be.true;
-                    console.log(assert);
-                    done();
-                });
+        model.getAccessToken(token,
+            function(err, result){
+                var assert = (err === null).should.be.true;
+                console.log(assert);
+                if (result){
+                    model.getAccessToken(token,
+                        function(err, result){
+                            assert = result.cached.should.be.true;
+                            console.log(assert);
+                            done();
+                        }
+                    );
+                }
             }
-        });
+        );
     });
 });
